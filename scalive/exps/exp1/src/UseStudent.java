a75c32dfbe2615afb158e052f059c196c2b7311c
SugamChaudharry <nextgenachieverss@gmail.com>
2024-08-22 07:35:03 +0530

    oops basic examples

---
+++ scalive/exps/exp1/src/UseStudent.java
@@ -0,0 +1,11 @@
+public class UseStudent {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.roll = 10;
+        s.name = "sugam";
+        s.per = 90.0;
+        System.out.println("Roll = "+s.roll);
+        System.out.println("name = "+s.name);
+        System.out.println("per = "+s.per);
+    }
+}
